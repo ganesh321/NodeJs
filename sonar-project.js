3e674f951434abdfdcb96c87c4375f1004985979
ganesh321 <31589144+ganesh321@users.noreply.github.com>
2021-12-21 11:02:39 +0400

    sonar
    sonar

--- sonar-project.js
+++ sonar-project.js
@@ -6,7 +6,7 @@ sonarqubeScanner({
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.login': 'admin',
-	    'sonar.password': 'admin',
+	    'sonar.password': '2b0eaa243a26a1a19bb0361f4ff4446072071a0e',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
